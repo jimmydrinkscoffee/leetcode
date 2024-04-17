a49352d1ddbe2846dbdc0d46079e2aefd1c3fc88
balconyhotmilk <jimmyvo.05122002@gmail.com>
2024-04-18 06:36:37 +0700

    Largest Rectangle in Histogram

--- go/Largest_Rectangle_in_Histogram.go
+++ go/Largest_Rectangle_in_Histogram.go
@@ -1,5 +1,43 @@
 package main
 
-func largestRectangleArea(heights []int) int {
-	return 0
+type stack struct {
+	v []int
+}
+
+func (stk *stack) top() int {
+	return stk.v[len(stk.v)-1]
+}
+
+func (stk *stack) push(v int) {
+	stk.v = append(stk.v, v)
+}
+
+func (stk *stack) pop() int {
+	v := stk.top()
+	stk.v = stk.v[:len(stk.v)-1]
+	return v
+}
+
+func (stk *stack) empty() bool {
+	return len(stk.v) <= 0
+}
+
+func largestRectangleArea(ht []int) int {
+	ht = append(ht, 0)
+	ans := 0
+	stk := &stack{
+		v: []int{},
+	}
+	for i, h := range ht {
+		for !stk.empty() && h < ht[stk.top()] {
+			top := stk.pop()
+			l := i
+			if !stk.empty() {
+				l = i - stk.top() - 1
+			}
+			ans = max(ans, ht[top]*l)
+		}
+		stk.push(i)
+	}
+	return ans
 }
