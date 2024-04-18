71225164f154fb43c350db9d3f5fe9608e7f1e54
balconyhotmilk <jimmyvo.05122002@gmail.com>
2024-04-18 07:00:08 +0700

    Maximal Rectangle

--- go/Largest_Rectangle_in_Histogram.go
+++ go/Largest_Rectangle_in_Histogram.go
@@ -1,43 +1,20 @@
 package main
 
-type stack struct {
-	v []int
-}
-
-func (stk *stack) top() int {
-	return stk.v[len(stk.v)-1]
-}
-
-func (stk *stack) push(v int) {
-	stk.v = append(stk.v, v)
-}
-
-func (stk *stack) pop() int {
-	v := stk.top()
-	stk.v = stk.v[:len(stk.v)-1]
-	return v
-}
-
-func (stk *stack) empty() bool {
-	return len(stk.v) <= 0
-}
-
 func largestRectangleArea(ht []int) int {
 	ht = append(ht, 0)
 	ans := 0
-	stk := &stack{
-		v: []int{},
-	}
+	var stk []int
 	for i, h := range ht {
-		for !stk.empty() && h < ht[stk.top()] {
-			top := stk.pop()
+		for len(stk) > 0 && h < ht[stk[len(stk)-1]] {
+			top := stk[len(stk)-1]
+			stk = stk[:len(stk)-1]
 			l := i
-			if !stk.empty() {
-				l = i - stk.top() - 1
+			if len(stk) > 0 {
+				l = i - stk[len(stk)-1] - 1
 			}
 			ans = max(ans, ht[top]*l)
 		}
-		stk.push(i)
+		stk = append(stk, i)
 	}
 	return ans
 }
--- go/Maximal_Rectangle.go
+++ go/Maximal_Rectangle.go
@@ -1,5 +1,17 @@
 package main
 
 func maximalRectangle(matrix [][]byte) int {
-	return 0
+	ans := 0
+	h := make([]int, len(matrix[0]))
+	for _, r := range matrix {
+		for i, c := range r {
+			if c == '1' {
+				h[i]++
+			} else {
+				h[i] = 0
+			}
+		}
+		ans = max(ans, largestRectangleArea(h))
+	}
+	return ans
 }
