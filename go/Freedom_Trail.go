802464376e57e41e2a24c3bb1bb1fc0c3d9972ad
balconyhotmilk <jimmyvo.05122002@gmail.com>
2024-04-27 18:49:05 +0700

    Freedom Trail

--- go/Freedom_Trail.go
+++ go/Freedom_Trail.go
@@ -1,18 +1,13 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 func findRotateSteps(rg string, key string) int {
 	m := make(map[byte][]int)
 	for i := 0; i < len(rg); i++ {
 		m[rg[i]] = append(m[rg[i]], i)
-	}
-	dp := make([][]int, len(key))
-	for i := 0; i < len(key); i++ {
-		dp[i] = make([]int, len(rg))
-		for j := 0; j < len(rg); j++ {
-			dp[i][j] = -1
-		}
 	}
 	abs := func(n int) int {
 		if n >= 0 {
@@ -20,22 +15,22 @@ func findRotateSteps(rg string, key string) int {
 		}
 		return -n
 	}
-	var mem func(k, r int) int
-	mem = func(k, r int) int {
-		if k == len(key) {
-			return 0
+	prev, cur := make([]int, len(rg)+1), make([]int, len(rg)+1)
+	for k := len(key) - 1; k >= 0; k-- {
+		for r := len(rg) - 1; r >= 0; r-- {
+			if k > 0 && rg[r] != key[k-1] {
+				continue
+			}
+			if key[k] == rg[r] {
+				cur[r] = 1 + prev[r]
+				continue
+			}
+			cur[r] = math.MaxInt32
+			for _, nx := range m[key[k]] {
+				cur[r] = min(cur[r], 1+min(abs(r-nx), len(rg)-nx+r, len(rg)-r+nx)+prev[nx])
+			}
 		}
-		if dp[k][r] != -1 {
-			return dp[k][r]
-		}
-		if key[k] == rg[r] {
-			dp[k][r] = 1 + mem(k+1, r)
-		}
-		dp[k][r] = math.MaxInt32
-		for _, nx := range m[key[k]] {
-			dp[k][r] = min(dp[k][r], 1+min(abs(r-nx), len(rg)-nx+r, len(rg)-r+nx)+mem(k+1, nx))
-		}
-		return dp[k][r]
+		copy(prev, cur)
 	}
-	return mem(0, 0)
+	return cur[0]
 }
