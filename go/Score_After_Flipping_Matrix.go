a3b6bf721f41df70e20bd378fa02eea58656b1cb
balconyhotmilk <jimmyvo.05122002@gmail.com>
2024-05-13 09:59:11 +0700

    Score After Flipping Matrix

--- go/Score_After_Flipping_Matrix.go
+++ go/Score_After_Flipping_Matrix.go
@@ -9,27 +9,13 @@ func matrixScore(grid [][]int) int {
 			}
 		}
 	}
-	v := make([]int, c)
+	ans := 0
 	for i := 0; i < r; i++ {
 		cnt := 0
 		for j := 0; j < c; j++ {
-			if grid[j][i] == 1 {
-				cnt++
-			} else {
-				cnt--
-			}
+			cnt += grid[j][i]
 		}
-		for j := 0; j < c; j++ {
-			n := grid[j][i]
-			if cnt < 0 {
-				n = 1 - n
-			}
-			v[j] = v[j]*2 + n
-		}
-	}
-	ans := 0
-	for _, n := range v {
-		ans += n
+		ans = ans*2 + max(cnt, c-cnt)
 	}
 	return ans
 }
