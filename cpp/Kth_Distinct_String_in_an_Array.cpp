babfff0ac8e5f4db94570f12bf27f1fdb1c76d87
jimmydrinkscoffee <jimmyvo.05122002@gmail.com>
2024-08-05 07:16:43 +0700

    Kth Distinct String in an Array

---
+++ cpp/Kth_Distinct_String_in_an_Array.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  string kthDistinct(vector<string> &arr, int k) {
+    unordered_map<string, int> m;
+    for (auto s : arr) {
+      m[s]++;
+    }
+    for (auto s : arr) {
+      if (m[s] == 1 && --k == 0) {
+        return s;
+      }
+    }
+    return "";
+  }
+};
